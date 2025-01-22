ebf00354554d088bbc4b012fe9b4b78635d72676
unknown <11Rahulsinghrawat@gmail.com>
2025-01-22 15:58:22 +0530

    removed backend to different directory

--- server/routes/Chat.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getUserChatById } = require('../controllers/ChatContoller');
-const { firebasePushNotification } = require('../utils/firebaseNotification');
-const router = express.Router();
-
-router.get('/getUserMessage',getUserChatById);
-
-module.exports = router
